d996e3dc4d2dd18678c67624e372ffe44c4d0eb7
kzryzstof <kzryzstof@proton.me>
2025-05-10 12:35:22 -0400

    .

--- devops/terraform/resources/app_runner_service.tf
+++ devops/terraform/resources/app_runner_service.tf
@@ -1,31 +1,6 @@
-resource "aws_apprunner_service" "default" {
-  service_name = local.service_name
-
-  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto.arn
-
-  source_configuration {
-    
-    image_repository {
-
-      image_configuration {
-        port = "8080"
-      }
-
-      image_identifier      = var.image_tag
-      image_repository_type = "ECR"
-    }
-
-    authentication_configuration {
-      access_role_arn = aws_iam_role.app_runner.arn
-    }
-    
-    auto_deployments_enabled = false
-  }
-  
-  tags = local.tags
-}
-
-//  IAM
+//  --------------------------------------------------------------------------------------------------------------------
+//  IAM Role
+//  --------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role" "app_runner_role" {
   name = "app_runner_role"
 
@@ -48,6 +23,52 @@ resource "aws_iam_role" "app_runner_role" {
   tags = local.tags
 }
 
+//  --------------------------------------------------------------------------------------------------------------------
+//  App Runner resource
+//  --------------------------------------------------------------------------------------------------------------------
+resource "aws_apprunner_service" "default" {
+  service_name = local.service_name
+
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto.arn
+
+  source_configuration {
+    
+    image_repository {
+
+      image_configuration {
+        port = "8080"
+      }
+
+      image_identifier      = var.image_tag
+      image_repository_type = "ECR"
+    }
+
+    authentication_configuration {
+      access_role_arn = aws_iam_role.app_runner_role.arn
+    }
+    
+    auto_deployments_enabled = false
+  }
+  
+  tags = local.tags
+}
+
+//  --------------------------------------------------------------------------------------------------------------------
+//  Scaling.
+//  --------------------------------------------------------------------------------------------------------------------
+resource "aws_apprunner_auto_scaling_configuration_version" "auto" {
+  auto_scaling_configuration_name = "default_scaling"
+  min_size = 1
+  max_size = 1
+}
+
+resource "aws_apprunner_default_auto_scaling_configuration_version" "default" {
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto.arn
+}
+
+//  --------------------------------------------------------------------------------------------------------------------
+//  App Runner permissions
+//  --------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_policy" "ecr_access" {
   name        = "app_runner_ecr_access_policy"
   description = "Policy to allow ECR access for App Runner role"
@@ -79,15 +100,4 @@ resource "aws_iam_role_policy_attachment" "app_runner" {
 resource "aws_iam_role_policy_attachment" "app_runner_ecr_access" {
   role       = aws_iam_role.app_runner_role.name
   policy_arn = aws_iam_policy.ecr_access.arn
-}
-
-//  Scaling.
-resource "aws_apprunner_auto_scaling_configuration_version" "auto" {
-  auto_scaling_configuration_name = "default_scaling"
-  min_size = 1
-  max_size = 1
-}
-
-resource "aws_apprunner_default_auto_scaling_configuration_version" "default" {
-  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto.arn
 }
