e4b8b7867da38be62a25cf324486a866da5f6c78
kzryzstof <kzryzstof@proton.me>
2025-05-12 21:47:31 -0400

    Correction.

--- devops/terraform/resources/dynamo_db_weather_grain_table.tf
+++ devops/terraform/resources/dynamo_db_weather_grain_table.tf
@@ -1,32 +1,17 @@
 resource "aws_dynamodb_table" "data_grain" {
   name           = local.resources_names.dynamodb_tables.data_grain
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "GrainIdN"
-  range_key      = "GrainTypeString"
+  hash_key       = "GrainReference"
+  range_key      = "GrainType"
 
   attribute {
-    name = "GrainIdN"
+    name = "GrainReference"
     type = "S"
   }
 
   attribute {
-    name = "GrainTypeString"
+    name = "GrainType"
     type = "S"
-  }
-
-  # Optional but recommended for performance
-  attribute {
-    name = "GrainIdS"
-    type = "S"  # String type
-  }
-
-  global_secondary_index {
-    name               = "GrainIdSIndex"
-    hash_key           = "GrainIdS"
-    range_key          = "GrainTypeString"
-    projection_type    = "ALL"
-    write_capacity     = 5
-    read_capacity      = 5
   }
 
   ttl {
